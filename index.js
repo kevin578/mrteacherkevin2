feb494da717390517a528de5bae114fe028e4227
kevin578 <kevinbriggs1@gmail.com>
2018-06-02 07:36:23 -0400

    can log in with backend

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const router = require('express').Router();
+const authRoutes = require('./routes/authRoutes');
+
+require('./models/user')
+require('./services/passport');
+
+mongoose.connect(process.env.MONGO_URI)
 
 const app = express();
 
-app.get('/', (req,res)=> {
+app.use(cookieSession({
+    maxAge:  30 /*days*/ * 24 /*hours*/ *  60 /*minutes*/ *  60 /*seconds*/ *  1000 /*milliseconds*/,
+    keys: [process.env.COOKIE_KEY]
+}))
 
-})
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(authRoutes);
+
+
 
 const PORT = process.env.PORT || 5000
 
