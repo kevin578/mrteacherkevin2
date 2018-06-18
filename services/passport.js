b55883edf0065002273a6a3df1953e59f22fb1a0
kevin578 <kevinbriggs1@gmail.com>
2018-06-18 08:28:25 -0400

    tried new callback

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,7 @@ passport.deserializeUser(async (id, done)=> {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback',
+    callbackURL: 'https://mrteacherkevin.herokuapp.com/auth/google/callback',
     proxy: true
 },
 async (accessToken, refreshToken, profile, done) => {
