ed3b8160f5b95781f5c24d7ac14981b798d036ed
kevin578 <kevinbriggs1@gmail.com>
2018-09-19 11:51:52 -0400

    simplified photoshop

--- client/src/components/Pages/Photoshop/Photoshop1.js
+++ client/src/components/Pages/Photoshop/Photoshop1.js
@@ -13,82 +13,29 @@ class Photoshop1 extends React.Component {
         {[
           <Body key="setup" title="Setup">
             <Header>Setup</Header>
-            <p>
-              Let's get setup to work with Photoshop. Make sure you meet the
-              requirements below and your project looks like the one below.
-            </p>
-            <Image src="https://res.cloudinary.com/ddrd9osrk/image/upload/v1535547982/mrteacherkevin/Photoshop/PhotoshopIntroImage.png" />
+            <Video src="https://res.cloudinary.com/ddrd9osrk/video/upload/v1535547964/mrteacherkevin/Photoshop/photoshopIntro.mp4" />
             <Header>Check that you have...</Header>
             <Checkbox>named your project</Checkbox>
             <Checkbox>set the width and height to 11 * 8.5.</Checkbox>
             <Checkbox>made the background white</Checkbox>
             <Button>Next</Button>
             <Header>Here's a video if you need help:</Header>
-            <Video src="https://res.cloudinary.com/ddrd9osrk/video/upload/v1535547964/mrteacherkevin/Photoshop/photoshopIntro.mp4" />
+           
           </Body>,
 
           <Body key="layers" title="Let's Get Our Bearings">
             <Header>Let's Get Our Bearings</Header>
-            <p>
-              Use this image to answer questions. There is a video below if you
-              need help.
-            </p>
-            <Image src="https://res.cloudinary.com/ddrd9osrk/image/upload/v1535549339/mrteacherkevin/Photoshop/layers_screenshot.png" />
-            <Question
-              choices={["tool options", "history", "new layer", "delete layer"]}
-              correctAnswer="new layer"
-            >
-              What does number 1 point to?
-            </Question>
-
-            <Question
-              choices={["tool options", "history", "new layer", "delete layer"]}
-              correctAnswer="delete layer"
-            >
-              What does number 2 point to?
-            </Question>
-
-            <Question
-              choices={["tool options", "history", "new layer", "delete layer"]}
-              correctAnswer="history"
-            >
-              What does number 3 point to?
-            </Question>
-
-            <Question
-              choices={["tool options", "history", "new layer", "delete layer"]}
-              correctAnswer="tool options"
-            >
-              What does number 4 point to?
-            </Question>
-
-            <Question
-              choices={["Green", "Red", "Purple", "Background"]}
-              correctAnswer="Red"
-            >
-              Which layer would not be visible?
-            </Question>
-
-            <Question
-              choices={["Green", "Red", "Purple", "Background"]}
-              correctAnswer="Green"
-            >
-              Which layer would be in front of all the other layers?
-            </Question>
-
-            <Button>Next</Button>
-            <Header>Use this video if you need help:</Header>
             <Video src="https://res.cloudinary.com/ddrd9osrk/video/upload/v1535549331/mrteacherkevin/Photoshop/layers.mp4" />
+            <Button>Next</Button>
+           
           </Body>,
 
           <Body key="again" title="Say Cheese">
             <Header>Say Cheese</Header>
-            <p>It's time to add some pictures!</p>
+            <Video src="https://res.cloudinary.com/ddrd9osrk/video/upload/v1535549980/mrteacherkevin/Photoshop/images.mp4" />
             <Header>Check that you have...</Header>
             <Checkbox>added at least two pictures.</Checkbox>
             <Button>Next</Button>
-            <Header>Use this video if you need help:</Header>
-            <Video src="https://res.cloudinary.com/ddrd9osrk/video/upload/v1535549980/mrteacherkevin/Photoshop/images.mp4" />
           </Body>,
 
           <Body key="selection" title="Selection Tools">
@@ -103,24 +50,6 @@ class Photoshop1 extends React.Component {
             <Checkbox>the magnetic lasso.</Checkbox>
             <Checkbox>the quick selection.</Checkbox>
             <Checkbox>the magic wand.</Checkbox>
-            <Question
-              correctAnswer="Command D"
-              choices={["Command D", "Shift D", "Alt D", "Double Click"]}
-            >
-              How do you deselect your selection?
-            </Question>
-
-            <Question
-              correctAnswer="Select > Inverse"
-              choices={[
-                "Layer > Inverse",
-                "Layer > Mask",
-                "Select > Isolate",
-                "Select > Inverse"
-              ]}
-            >
-              How do you inverse your selection?
-            </Question>
             <Button>Next</Button>
 
           </Body>,
@@ -133,16 +62,6 @@ class Photoshop1 extends React.Component {
             <Checkbox>changed the font.</Checkbox>
             <Checkbox>changed the font size.</Checkbox>
             <Checkbox>added a layer style.</Checkbox>
-            <Question
-              choices={[
-                "change the font property.",
-                "add a gradiant base.",
-                "select it.",
-                "emboss the finish."
-              ]}
-            >
-              If you want to add a gradiant to your text you need to first...
-            </Question>
             <Button>Next</Button>
           </Body>,
 
