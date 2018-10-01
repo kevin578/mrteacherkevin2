b9f9750b5df1c70e4150111be7728007d1f69ae9
kevin578 <kevinbriggs1@gmail.com>
2018-10-01 11:27:35 -0400

    switched photoshop videos to s3

--- client/src/components/Pages/Photoshop/Photoshop1.js
+++ client/src/components/Pages/Photoshop/Photoshop1.js
@@ -13,7 +13,7 @@ class Photoshop1 extends React.Component {
         {[
           <Body key="setup" title="Setup">
             <Header>Setup</Header>
-            <Video src="https://firebasestorage.googleapis.com/v0/b/mrteacherkevin-204010.appspot.com/o/Photoshop%2Fintro.mp4?alt=media&token=0cae5f86-2bc7-4c82-abac-6337580ca49b" />
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/Photoshop/intro.mp4" />
             <Header>Check that you have...</Header>
             <Checkbox>named your project</Checkbox>
             <Checkbox>set the width and height to 11 * 8.5.</Checkbox>
@@ -25,14 +25,14 @@ class Photoshop1 extends React.Component {
 
           <Body key="layers" title="Let's Get Our Bearings">
             <Header>Let's Get Our Bearings</Header>
-            <Video src="https://firebasestorage.googleapis.com/v0/b/mrteacherkevin-204010.appspot.com/o/Photoshop%2Flayers.mp4?alt=media&token=427d77c8-d24b-4b5a-8c69-3646841e7ecc" />
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/Photoshop/layers.mp4" />
             <Button>Next</Button>
            
           </Body>,
 
           <Body key = "resetessentials" title = "Reset Essentials">
             <Header>Reset Essentials</Header>
-            <Video src = "https://firebasestorage.googleapis.com/v0/b/mrteacherkevin-204010.appspot.com/o/Photoshop%2Fresetessentials.mp4?alt=media&token=e6ee0fd1-d091-4341-af92-00860dd703f9"/>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/Photoshop/resetessentials.mp4"/>
             <Header>I have...</Header>
             <Checkbox>deleted my tools and layers.</Checkbox>
             <Checkbox>gotten them back by using reset essentials.</Checkbox>
@@ -41,7 +41,7 @@ class Photoshop1 extends React.Component {
 
           <Body key="again" title="Say Cheese">
             <Header>Say Cheese</Header>
-            <Video src="https://firebasestorage.googleapis.com/v0/b/mrteacherkevin-204010.appspot.com/o/Photoshop%2Fimages.mp4?alt=media&token=3b3b45b9-f1f4-476c-967f-2cb0e3706312" />
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/Photoshop/images.mp4" />
             <Header>Check that you have...</Header>
             <Checkbox>added at least two pictures.</Checkbox>
             <Button>Next</Button>
@@ -52,7 +52,7 @@ class Photoshop1 extends React.Component {
             <p>
               Selections allow you to isolate certain parts of your project.
             </p>
-            <Video src="https://firebasestorage.googleapis.com/v0/b/mrteacherkevin-204010.appspot.com/o/Photoshop%2FselectionTools.mp4?alt=media&token=70428160-475f-4e57-b0f3-9b3e5743f103" />
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/Photoshop/selectionTools.mp4" />
             <Header>I have tried...</Header>
             <Checkbox>the lasso.</Checkbox>
             <Checkbox>the polygonal lasso.</Checkbox>
@@ -65,7 +65,7 @@ class Photoshop1 extends React.Component {
 
           <Body key = "freetransform" title = "Free Transform">
             <Header>Free Transform</Header>
-            <Video src = "https://firebasestorage.googleapis.com/v0/b/mrteacherkevin-204010.appspot.com/o/Photoshop%2Ftransformations.mp4?alt=media&token=c2b3f759-b4a9-468a-b2a9-2d2a3768863d" />
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/Photoshop/transformations.mp4" />
             <Header>I have...</Header>
             <Checkbox>used the Free Transform tool.</Checkbox>
             <Checkbox>tried at least one Image Adjustment.</Checkbox>
@@ -75,7 +75,7 @@ class Photoshop1 extends React.Component {
           <Body key="text" title="Text">
             <Header>Text</Header>
             <p>Time to add a little writing to your project.</p>
-            <Video src = "https://res.cloudinary.com/ddrd9osrk/video/upload/v1535556280/mrteacherkevin/Photoshop/text.mp4"/>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/Photoshop/text.mp4"/>
             <Header>Check that you have...</Header>
             <Checkbox>changed the font.</Checkbox>
             <Checkbox>changed the font size.</Checkbox>
@@ -90,12 +90,12 @@ class Photoshop1 extends React.Component {
             <Checkbox>have a font is a different font than Myriad Pro.</Checkbox>
             <Checkbox>use at least one of the Blending Options.</Checkbox>
             <Header>Once you have completed all the objectives, watch this video:</Header>
-            <Video src = "https://firebasestorage.googleapis.com/v0/b/mrteacherkevin-204010.appspot.com/o/Photoshop%2Fshare.mp4?alt=media&token=d0d1121a-bf3b-417a-b2c0-d2a09819a28b"/>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/Photoshop/share.mp4"/>
           </Body>
         ]}
       </Subject>
     );
-  }
+  }git 
 }
 
 export default Photoshop1;
