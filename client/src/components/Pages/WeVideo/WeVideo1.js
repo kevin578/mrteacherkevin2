e18a05669af39199b0aa339cfe8cf32fce3d498e
kevin578 <kevinbriggs1@gmail.com>
2018-10-02 11:31:44 -0400

    added first parts of Soundtrap

--- client/src/components/Pages/WeVideo/WeVideo1.js
+++ client/src/components/Pages/WeVideo/WeVideo1.js
@@ -11,14 +11,24 @@ class WeVideo1 extends React.Component {
     return (
       <Subject title="WeVideo" urlName="wevideo1">
         {[
-          <Body key="starting" title="Start something">
-            <Header>Sample Header</Header>
-            <p>Some Text</p>
+          <Body key="gettingStarted" title="Getting Started">
+            <Header>Getting Started</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/wevideo/weVideoIntro.mp4"/>
+            <Header>I have...</Header>
+            <Checkbox>created a new WeVideo account.</Checkbox>
+            <Checkbox>created a new project folder.</Checkbox>
+            <Checkbox>created a new video inside that project.</Checkbox>
+            <Checkbox>changed the name of my video.</Checkbox>
             <Button>Next</Button>
           </Body>,
 
-          <Body key="again" title="Do somthing again">
-            <Header>Do something again</Header>
+          <Body key="addMedia" title="Adding Media">
+            <Header>Adding Media</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/wevideo/addMedia.mp4"/>
+            <Header>I have...</Header>
+            <Checkbox>added my media into the project folder.</Checkbox>
+            <Checkbox>added into the actual movie timeline.</Checkbox>
+            <Checkbox>clipped it down so it everything is nice and snappy.</Checkbox>
             <Button>Next</Button>
           </Body>
         ]}
