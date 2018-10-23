807e0c5c62afa7790a9142e624e5e5978c0a429c
kevin578 <kevinbriggs1@gmail.com>
2018-10-23 11:28:57 -0400

    added google slides

--- client/src/components/Pages/GoogleApps/GoogleApps2.js
+++ client/src/components/Pages/GoogleApps/GoogleApps2.js
@@ -9,22 +9,53 @@ import Button from "../../Subject/Button";
 class GoogleApps2 extends React.Component {
   render() {
     return (
-      <Subject title="Sample Page" urlName="sampleURL">
+      <Subject title="Google Apps" urlName="googleapps2">
         {[
-          <Body key="starting" title="Start something">
-            <Header>Sample Header</Header>
-            <p>Some Text</p>
+          <Body key="intro" title="Getting Started">
+            <Header>Get Started</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/slidesintro.mp4"/>
+            <Header>I have...</Header>
+            <Checkbox>created a new Google Slide.</Checkbox>
+            <Checkbox>given it a name.</Checkbox>
+            <Checkbox>given it a blank layout.</Checkbox>
             <Button>Next</Button>
           </Body>,
 
-          <Body key="again" title="Do somthing again">
-            <Header>Do something again</Header>
+          <Body key="background" title="Backgrounds">
+            <Header>Backgrounds</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/backgrounds.mp4"/>
+            <Header>I have...</Header>
+            <Checkbox>used a plain solid color for a background.</Checkbox>
+            <Checkbox>used a custom gradient for a background.</Checkbox>
+            <Checkbox>used an image for a background.</Checkbox>
             <Button>Next</Button>
-          </Body>
+          </Body>,
+
+          <Body key = "text" title= "Text">
+          <Header>Text</Header>
+          <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/slidesText.mp4"/>
+          <Header>I have...</Header>
+          <Checkbox>added Word Art.</Checkbox>
+          <Checkbox>added a text box.</Checkbox>
+          <Checkbox>changed the font.</Checkbox>
+          <Checkbox>changed the font color.</Checkbox>
+          <Button>Next</Button>
+          </Body>,
+
+          <Body key = "images" title = "Shapes and Images">
+            <Header>Shapes and Images</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/slidesImage.mp4"/>
+            <Header>I have...</Header>
+            <Checkbox>added an image.</Checkbox>
+            <Checkbox>added a shape.</Checkbox>
+          </Body>,
+
+          
+
         ]}
       </Subject>
     );
   }
 }
 
-export default GoogleChrome2;
+export default GoogleApps2;
