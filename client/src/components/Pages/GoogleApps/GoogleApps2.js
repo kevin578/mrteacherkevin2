bd431cd6c253a0d05f43692e3d5d56c6a88c0840
kevin578 <kevinbriggs1@gmail.com>
2018-11-01 10:07:58 -0400

    slides section is done

--- client/src/components/Pages/GoogleApps/GoogleApps2.js
+++ client/src/components/Pages/GoogleApps/GoogleApps2.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Subject from "../../Subject/Subject";
 import { Body, Header, Video } from "../../Subject/SubjectStyles";
+import ProjectSubmission from "../../Subject/ProjectSubmission";
 import Checkbox from "../../Subject/Checkbox";
 import Question from "../../Subject/Quiz";
 import TextEditor from "../../Subject/TextEditor";
@@ -13,7 +14,7 @@ class GoogleApps2 extends React.Component {
         {[
           <Body key="intro" title="Getting Started">
             <Header>Get Started</Header>
-            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/slidesintro.mp4"/>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/slidesintro.mp4" />
             <Header>I have...</Header>
             <Checkbox>created a new Google Slide.</Checkbox>
             <Checkbox>given it a name.</Checkbox>
@@ -23,7 +24,7 @@ class GoogleApps2 extends React.Component {
 
           <Body key="background" title="Backgrounds">
             <Header>Backgrounds</Header>
-            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/backgrounds.mp4"/>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/backgrounds.mp4" />
             <Header>I have...</Header>
             <Checkbox>used a plain solid color for a background.</Checkbox>
             <Checkbox>used a custom gradient for a background.</Checkbox>
@@ -31,41 +32,64 @@ class GoogleApps2 extends React.Component {
             <Button>Next</Button>
           </Body>,
 
-          <Body key = "text" title= "Text">
-          <Header>Text</Header>
-          <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/slidesText.mp4"/>
-          <Header>I have...</Header>
-          <Checkbox>added Word Art.</Checkbox>
-          <Checkbox>added a text box.</Checkbox>
-          <Checkbox>changed the font.</Checkbox>
-          <Checkbox>changed the font color.</Checkbox>
-          <Button>Next</Button>
+          <Body key="text" title="Text">
+            <Header>Text</Header>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/slidesText.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>added Word Art.</Checkbox>
+            <Checkbox>added a text box.</Checkbox>
+            <Checkbox>changed the font.</Checkbox>
+            <Checkbox>changed the font color.</Checkbox>
+            <Button>Next</Button>
           </Body>,
 
-          <Body key = "images" title = "Shapes and Images">
+          <Body key="images" title="Shapes and Images">
             <Header>Shapes and Images</Header>
-            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/slidesImage.mp4"/>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/images.mp4" />
             <Header>I have...</Header>
             <Checkbox>added an image.</Checkbox>
             <Checkbox>added a shape.</Checkbox>
+            <Checkbox>changed the stroke and fill on a shape.</Checkbox>
+            <Checkbox>cropped an image.</Checkbox>
+            <Checkbox>used an image option on either an image or a shape.</Checkbox>
+            <Button>Next</Button>
           </Body>,
-          
-          <Body key = "share" title = "Share">
-            <Header>Share</Header>
+
+          <Body key="transitions" title="Transitions">
+            <Header>Transitions</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/transitions.mp4"/>
             <Header>I have...</Header>
-            <Checkbox>a story that consists of exactly 5 slides.</Checkbox>
+            <Checkbox>added a transition</Checkbox>
+            <Button>Next</Button>
+          </Body>,
+
+          <Body key = "animations" title = "Animations">
+            <Header>Animations</Header>
+            <Header>I have...</Header>
+            <Checkbox>added an animation</Checkbox>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/animations.mp4"/>
+            <Button>Next</Button>
+          </Body>,
+
+          <Body key="share" title="Share">
+            <Header>I have...</Header>
+            <Checkbox>a slideshow that consists of exactly 5 slides.</Checkbox>
             <Checkbox>one slide that has an image background.</Checkbox>
             <Checkbox>one slide that has a gradient background.</Checkbox>
             <Checkbox>text with a font different than Ariel.</Checkbox>
             <Checkbox>a shape with a different fill and line color.</Checkbox>
             <Checkbox>text in the speaker notes.</Checkbox>
-            <Checkbox>text that is not exactly what I'm going to say.</Checkbox>
+            <Checkbox>a small amount of text in my actual slideshow.</Checkbox>
             <Checkbox>an animation.</Checkbox>
             <Checkbox>a transition.</Checkbox>
             <Checkbox>an example of word art.</Checkbox>
-          </Body>
-          
+            <Header>How to share:</Header>
+            <Video src = ""/>
+            <Header>Share your amazing slideshow here:</Header>
+            <ProjectSubmission projectKey = "slideshowoff"/>
+            <Button>Submit</Button>
 
+          </Body>
         ]}
       </Subject>
     );
--- client/src/components/Pages/GoogleSlides/GoogleSlides1.js
+++ client/src/components/Pages/GoogleSlides/GoogleSlides1.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Subject from "./../../Subject/Subject";
+import Subject from "../../Subject/Subject";
 import { Body, Header, Video } from "../../Subject/SubjectStyles";
-import Checkbox from "./../../Subject/Checkbox";
+import ProjectSubmission from "../../Subject/ProjectSubmission";
+import Checkbox from "../../Subject/Checkbox";
 import Question from "../../Subject/Quiz";
 import TextEditor from "../../Subject/TextEditor";
 import Button from "../../Subject/Button";
@@ -9,17 +10,85 @@ import Button from "../../Subject/Button";
 class GoogleSlides1 extends React.Component {
   render() {
     return (
-      <Subject title="Google Slides" urlName="sampleURL">
+      <Subject title="Google Slides" urlName="googleslides1">
         {[
-          <Body key="starting" title="Start something">
-            <Header>Sample Header</Header>
-            <p>Some Text</p>
+          <Body key="intro" title="Getting Started">
+            <Header>Get Started</Header>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/slidesintro.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>created a new Google Slide.</Checkbox>
+            <Checkbox>given it a name.</Checkbox>
+            <Checkbox>given it a blank layout.</Checkbox>
             <Button>Next</Button>
           </Body>,
 
-          <Body key="again" title="Do somthing again">
-            <Header>Do something again</Header>
+          <Body key="background" title="Backgrounds">
+            <Header>Backgrounds</Header>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/backgrounds.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>used a plain solid color for a background.</Checkbox>
+            <Checkbox>used a custom gradient for a background.</Checkbox>
+            <Checkbox>used an image for a background.</Checkbox>
             <Button>Next</Button>
+          </Body>,
+
+          <Body key="text" title="Text">
+            <Header>Text</Header>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/slidesText.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>added Word Art.</Checkbox>
+            <Checkbox>added a text box.</Checkbox>
+            <Checkbox>changed the font.</Checkbox>
+            <Checkbox>changed the font color.</Checkbox>
+            <Button>Next</Button>
+          </Body>,
+
+          <Body key="images" title="Shapes and Images">
+            <Header>Shapes and Images</Header>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/images.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>added an image.</Checkbox>
+            <Checkbox>added a shape.</Checkbox>
+            <Checkbox>changed the stroke and fill on a shape.</Checkbox>
+            <Checkbox>cropped an image.</Checkbox>
+            <Checkbox>used the format options on either an image or a shape.</Checkbox>
+            <Button>Next</Button>
+          </Body>,
+
+          <Body key="transitions" title="Transitions">
+            <Header>Transitions</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/transitions.mp4"/>
+            <Header>I have...</Header>
+            <Checkbox>added a transition</Checkbox>
+            <Button>Next</Button>
+          </Body>,
+
+          <Body key = "animations" title = "Animations">
+            <Header>Animations</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/animations.mp4"/>
+            <Header>I have...</Header>
+            <Checkbox>added an animation</Checkbox>  
+            <Button>Next</Button>
+          </Body>,
+
+          <Body key="share" title="Share">
+            <Header>I have...</Header>
+            <Checkbox>a slideshow that consists of exactly 5 slides.</Checkbox>
+            <Checkbox>one slide that has an image background.</Checkbox>
+            <Checkbox>one slide that has a gradient background.</Checkbox>
+            <Checkbox>text with a font different than Ariel.</Checkbox>
+            <Checkbox>a shape with a different fill and line color.</Checkbox>
+            <Checkbox>text in the speaker notes.</Checkbox>
+            <Checkbox>a small amount of text in my actual slideshow.</Checkbox>
+            <Checkbox>an animation.</Checkbox>
+            <Checkbox>a transition.</Checkbox>
+            <Checkbox>an example of word art.</Checkbox>
+            <Header>How to share:</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/googleApps/slides/share.mp4"/>
+            <Header>Share your amazing slideshow here:</Header>
+            <ProjectSubmission projectKey = "slideshowoff"/>
+            <Button>Submit</Button>
+
           </Body>
         ]}
       </Subject>
