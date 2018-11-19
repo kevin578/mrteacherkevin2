0e82ce220d0cab8c8165f16b16673cb4b277e7d2
kevin578 <kevinbriggs1@gmail.com>
2018-11-19 11:21:40 -0500

    added second part of scratch

--- client/src/components/Pages/Scratch/Scratch2.js
+++ client/src/components/Pages/Scratch/Scratch2.js
@@ -12,37 +12,47 @@ class Scratch2 extends React.Component {
       <Subject title="Scratch" urlName="scratch2">
         {[
           <Body key="ifthenelse" title="If then else">
-            <Header>If then else</Header>
+            <Header></Header>
             <p>Make a pet that wants to be pet. When you hover your mouse over it, they become happy. Like so:</p>
             <Image src = "https://res.cloudinary.com/ddrd9osrk/image/upload/v1533829525/ifthenelse.gif" />
             <Header>I have...</Header>
             <Checkbox>a pet that says one thing when my mouse is not touching it,</Checkbox>
             <Checkbox>and says another thing when my mouse is touching it.</Checkbox>
-            <Header>Answer these questions before moving on:</Header>
-            <Question
-              type="mc"
-              choices={["circle", "trapezoid", "square", "triangle"]}
-              correctAnswer = "trapezoid"
-            >
-              What shape are boolean blocks?
-            </Question>
-            <Question correctAnswer = "2">
-              How many values can a boolean block have?
-            </Question>
             <Button>Next</Button>
           </Body>,
+          <Body key = "changeXY" title = "Under Control">
+            <Header>I can...</Header>
+            <Checkbox>use the arrow keys to change the X and Y position of my characters.</Checkbox>
+            <Button>Next</Button>
+          </Body>,
+          <Body key = "moveBlock" title = "Step Up">
+          <Header>I can...</Header>
+          <Checkbox>use the move block to make my sprite move.</Checkbox>
+          <Checkbox>use the rotation blocks to change the direction of the movemnt</Checkbox>
+          <Button>Next</Button>
+          </Body>,
+          <Body key = "variable" title = "Variables">
+          <Header>I have...</Header>
+          <Checkbox>used a variable in a meaningful way.</Checkbox>
+          <Button>Next</Button>
+          </Body>,
 
-          <Body key="moveWithKeys" title="Move with the Arrows" />,
-          <Body key="bounce" title="One Way then Antother" />,
-          <Body key="moveAndRotate" title="Rotation Rotation Rotation" />,
-          <Body key="value" title="Value Blocks" />,
-          <Body key="doubler" title="The Doubler" />,
-          <Body key="answer" title="Answer Me this" />,
-          <Body key="variables" title="Stopwatch" />,
-          <Body key="drums" title="Drum Machine" />,
-          <Body key="events" title="The Main Event" />,
-          <Body key="quiz" title="Quiz Game" />,
-          <Body key="quiz" title="Action Game" />,
+          <Body key = "events" title = "The Main Event">
+          <Header>I have...</Header>
+          <Checkbox>broadcasted an event and used a listener.</Checkbox>
+          <Button>Next</Button>
+          </Body>,
+          <Body key = "question" title = "Question Time">
+          <Header>I have...</Header>
+          <Checkbox>used the ask block.</Checkbox>
+          <Checkbox>used the answer block in a meaningful way.</Checkbox>
+          <Button>Next</Button>
+          </Body>,
+          <Body key = "clones" title = "Attack of the Clones">
+          <Header>I have...</Header>
+          <Checkbox>used a clone</Checkbox>
+          <Button>Next</Button>
+          </Body>
           
 
 
