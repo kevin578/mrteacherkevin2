cd0c1c6a21485e6678b9bef073e0e4c94627e723
kevin578 <kevinbriggs1@gmail.com>
2019-02-12 14:45:46 -0500

    added new arduino videos

--- client/src/components/Pages/Arduino/Arduino1.js
+++ client/src/components/Pages/Arduino/Arduino1.js
@@ -21,13 +21,14 @@ class Arduino1 extends React.Component {
           </Body>,
 
           <Body key="dcmotor" title="DC Motor">
-            <Header>Do something again</Header>
+            <Header>DC Motor</Header>
             <Video src = "https://www.youtube.com/embed/zPnPCeU6W7E" videoPlayerType = "iframe"/>
             <Button>Next</Button>
           </Body>,
 
           <Body key="motordriver" title="Motor Driver">
             <Header>Motor Driver</Header>
+            <p>Note: If you have a Sparkfun Motor driver, you will be need to plug the STBY pin into 5V.</p>
             <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/arduino/motor_driver.mp4" />
             <Header>I have...</Header>
             <Checkbox>used a motor driver to power a motor</Checkbox>
@@ -35,7 +36,10 @@ class Arduino1 extends React.Component {
           </Body>,
 
           <Body key="lcdscreen" title="LCD Screen">
-            <Header>Do something again</Header>
+            <Header>LCD Screen</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/arduino/lcd.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>displayed a message on my LCD screen.</Checkbox>
             <Button>Next</Button>,
           </Body>,
 
@@ -47,8 +51,10 @@ class Arduino1 extends React.Component {
           </Body>,
 
           <Body key="neopixels" title="NeoPixels">
-            <Header>Sample Header</Header>
-            <p>Some Text</p>
+            <Header>Neopixels</Header>
+            <Video src =  "https://s3.us-east-2.amazonaws.com/mrtk-videos/arduino/neopixel.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>made my NeoPixel light up.</Checkbox>
             <Button>Next</Button>
           </Body>,
 
