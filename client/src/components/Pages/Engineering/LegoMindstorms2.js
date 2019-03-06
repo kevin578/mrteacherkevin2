4338d568a4666ddbe20b40c147b9b51d26e6c013
kevin578 <kevinbriggs1@gmail.com>
2019-03-06 10:08:52 -0500

    added mindstorms

--- client/src/components/Pages/Engineering/LegoMindstorms2.js
+++ client/src/components/Pages/Engineering/LegoMindstorms2.js
@@ -13,16 +13,36 @@ class LegoMindstorms2 extends React.Component {
         {[
           <Body key="lineFollower" title="Line Follower">
             <Header>Line Follower</Header>
-            <Video src = "https://www.youtube.com/watch?v=ye3MhVA9Rhs" iframe/>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/mindstorms/line_follower.mp4" />
             <Header>I have...</Header>
-            <Checkbox>made my robot follow line.</Checkbox>
+            <Checkbox>made my robot follow a straight line.</Checkbox>
+            <Checkbox>made my robot follow a curved line.</Checkbox>
             <Button>Next</Button>
           </Body>,
 
           <Body key="straigh-line" title="Straight Line">
             <Header>Straight Line</Header>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/mindstorms/gyro.mp4" />
             <Header>I have...</Header>
-            <Checkbox>made my robot go straight.</Checkbox>
+            <Checkbox>
+              made my robot go straight using the gyro sensor.
+            </Checkbox>
+            <Checkbox>
+              mae my robot go straight at a different heading than 0
+            </Checkbox>
+            <Button>Next</Button>
+          </Body>,
+
+          <Body key="my-block" title="My Blocks">
+            <Header>My Blocks</Header>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/mindstorms/myBlock.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>
+              created a myBlock.
+            </Checkbox>
+            <Checkbox>
+              created a myBlock with parameters.
+            </Checkbox>
             <Button>Next</Button>
           </Body>
         ]}
