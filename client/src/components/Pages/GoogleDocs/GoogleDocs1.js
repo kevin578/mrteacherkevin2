a77a681eef71a89a43217993b206bc57f3f6d120
kevin578 <kevinbriggs1@gmail.com>
2019-03-11 12:10:55 -0400

    added google docs

--- client/src/components/Pages/GoogleDocs/GoogleDocs1.js
+++ client/src/components/Pages/GoogleDocs/GoogleDocs1.js
@@ -9,18 +9,38 @@ import Button from "../../Subject/Button";
 class GoogleDocs1 extends React.Component {
   render() {
     return (
-      <Subject title="Sample Page" urlName="sampleURL">
+      <Subject title="Google Docs" urlName="googledocs1">
         {[
-          <Body key="starting" title="Start something">
-            <Header>Sample Header</Header>
-            <p>Some Text</p>
+          <Body key="starting" title="Getting Started">
+            <Header>Getting Started</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/google-docs/google-docs-intro.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>created a new Google Doc.</Checkbox>
+            <Checkbox>named my project.</Checkbox>
             <Button>Next</Button>
           </Body>,
 
-          <Body key="again" title="Do somthing again">
-            <Header>Do something again</Header>
+          <Body key="fonts" title="Fonts">
+            <Header>Fonts</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/google-docs/google-docs-text.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>changed my font type.</Checkbox>
+            <Checkbox>changed my font size.</Checkbox>
+            <Checkbox>changed my font color.</Checkbox>
+            <Checkbox>centered my title.</Checkbox>
+            <Checkbox>left-aligned my other text.</Checkbox>
+            <Checkbox>used either a bold, italic, or underline</Checkbox>
             <Button>Next</Button>
-          </Body>
+          </Body>,
+
+          <Body key="images" title="Images">
+            <Header>Images</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/google-docs/google-docs-images.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>added an image.</Checkbox>
+            <Checkbox>add a drawing.</Checkbox>
+            <Button>Next</Button>
+          </Body>,
         ]}
       </Subject>
     );
