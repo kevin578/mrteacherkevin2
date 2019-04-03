2aeb0249b731738091efa642cf003b36d599cfff
kevin578 <kevinbriggs1@gmail.com>
2019-04-03 13:20:33 -0400

    updated google docs

--- client/src/components/Pages/GoogleDocs/GoogleDocs1.js
+++ client/src/components/Pages/GoogleDocs/GoogleDocs1.js
@@ -2,6 +2,7 @@ import React from "react";
 import Subject from "./../../Subject/Subject";
 import { Body, Header, Video } from "../../Subject/SubjectStyles";
 import Checkbox from "./../../Subject/Checkbox";
+import ProjectSubmission from "./../../Subject/ProjectSubmission";
 import Question from "../../Subject/Quiz";
 import TextEditor from "../../Subject/TextEditor";
 import Button from "../../Subject/Button";
@@ -13,7 +14,7 @@ class GoogleDocs1 extends React.Component {
         {[
           <Body key="starting" title="Getting Started">
             <Header>Getting Started</Header>
-            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/google-docs/google-docs-intro.mp4" />
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/google-docs/google-docs-intro.mp4" />
             <Header>I have...</Header>
             <Checkbox>created a new Google Doc.</Checkbox>
             <Checkbox>named my project.</Checkbox>
@@ -22,7 +23,7 @@ class GoogleDocs1 extends React.Component {
 
           <Body key="fonts" title="Fonts">
             <Header>Fonts</Header>
-            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/google-docs/google-docs-text.mp4" />
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/google-docs/google-docs-text.mp4" />
             <Header>I have...</Header>
             <Checkbox>changed my font type.</Checkbox>
             <Checkbox>changed my font size.</Checkbox>
@@ -35,12 +36,33 @@ class GoogleDocs1 extends React.Component {
 
           <Body key="images" title="Images">
             <Header>Images</Header>
-            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/google-docs/google-docs-images.mp4" />
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/google-docs/google-docs-images.mp4" />
             <Header>I have...</Header>
             <Checkbox>added an image.</Checkbox>
             <Checkbox>add a drawing.</Checkbox>
             <Button>Next</Button>
           </Body>,
+
+          <Body key="share" title="Share">
+            <Header>Share</Header>
+            <p>Write a story or essay about a subject of your choosing.</p>
+            <Header>Make sure the title...</Header>
+            <Checkbox>is centered</Checkbox>
+            <Checkbox>has a different font type than the body.</Checkbox>
+            <Checkbox>has a larger font than the body.</Checkbox>
+            <Checkbox>has a different font color than the body.</Checkbox>
+            <Header>Make sure the body...</Header>
+            <Checkbox>is left aligned.</Checkbox>
+            <Checkbox>has a different font type than the title.</Checkbox>
+            <Checkbox>has a smaller font than the title.</Checkbox>
+            <Checkbox>has a different font color than the title.</Checkbox>
+            <Checkbox>contains an image.</Checkbox>
+            <Header>How to share...</Header>
+            <Video src="https://s3.us-east-2.amazonaws.com/mrtk-videos/google-docs/docs1share.mp4" />
+            <Header>Share your story here:</Header>
+            <ProjectSubmission projectKey="firstGoogleDocsStory" />
+            <Button>Next</Button>
+          </Body>
         ]}
       </Subject>
     );
