16e241a4c2189f345566e9e50a3f65189057911c
kevin578 <kevinbriggs1@gmail.com>
2019-04-09 10:32:13 -0400

    added buttons

--- client/src/components/Pages/Photoshop/Photoshop2.js
+++ client/src/components/Pages/Photoshop/Photoshop2.js
@@ -9,16 +9,21 @@ import Button from "../../Subject/Button";
 class Photoshop2 extends React.Component {
   render() {
     return (
-      <Subject title="Photoshop" urlName="sampleURL">
+      <Subject title="Photoshop" urlName="photoshop2">
         {[
-          <Body key="starting" title="Start something">
-            <Header>Sample Header</Header>
-            <p>Some Text</p>
+          <Body key="starting" title="Setup">
+            <Header>Setup</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/Photoshop/button_setup.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>made a 70mm circle.</Checkbox>
             <Button>Next</Button>
           </Body>,
 
-          <Body key="again" title="Do somthing again">
-            <Header>Do something again</Header>
+          <Body key="clippingmask" title="Clipping Masks">
+            <Header>Clipping Mask</Header>
+            <Video src = "https://s3.us-east-2.amazonaws.com/mrtk-videos/Photoshop/clipping+mask.mp4" />
+            <Header>I have...</Header>
+            <Checkbox>used clipping mask.</Checkbox>
             <Button>Next</Button>
           </Body>
         ]}
