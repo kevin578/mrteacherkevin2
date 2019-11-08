7de25233195928562b265cce507f86c9d94cd155
Kevin Briggs <kevinbriggs1@gmail.com>
2019-11-07 19:38:48 -0500

    view added for profile

--- client/src/components/shared/utilityComponents.js
+++ client/src/components/shared/utilityComponents.js
@@ -1,13 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
+
+
 export const Button = styled.div`
   width: 120px;
   height: 40px;
   border-radius: 3px;
-  background: #ff9000;
+  background: ${(props)=> props.background? props.background : "#ff9000" };
   text-align: center;
   line-height: 40px;
+  font-weight: 600;
+  color: #fff;
+  cursor: pointer;
   &:hover {
     box-shadow: 0px 0px 5px #fff;
   }
