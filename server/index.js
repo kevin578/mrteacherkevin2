56a6dc5b1a3f9a1f9ed3186d35aff44528265809
Kevin Briggs <kevinbriggs1@gmail.com>
2020-05-25 20:25:01 -0400

    updated scripts file

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 const app = express();
 
